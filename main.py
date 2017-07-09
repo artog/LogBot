ee4ba17a3f556458fa5a2718f43d47932ff27502
artog <adam.ingmansson@gmail.com>
2017-07-09 17:57:20 +0200

    Changed to a class and a proper CLI in which you can type commands

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
 from WarcraftLogApi import WarcraftLogApi
+from PollingThread import PollingThread
+from DiscordApi import DiscordApi
 import configparser
+import time
+from sys import exit
 
+class LogBot():
+
+    configFile = "config.ini"
+
+
+    def __init__(self):
+        print ("Starting LogBot")
+
+        config = configparser.ConfigParser()
+        config.read(self.configFile)
+
+        try:
+            self.discordApi = DiscordApi(config['discord'])
+            self.logApi = WarcraftLogApi(config['warcraftlogs'])
+        except Exception as e:
+            print("Error starting apis: {0}".format(repr(e)))
+            exit(1)
+
+    def printHelp(self):
+        print("----------- LogBot Help -----------")
+        print("git gud. noob.")
+        print(self.logApi.getFights("kFzACTYJbcmXLGqB"))
+        print(self.logApi.getReports(1499277098372).json())
+
+    def sendMessage(self, msg):
+        self.discordApi.sendMessage(msg)
+
+    def getChannel(self):
+        self.discordApi.getChannel()
+
+    def startPolling(self):
+        pass
+
+    def stopPolling(self):
+        pass
+
+    def startCLI(self):
+        while True:
+            parts = input("LogBot> ").split(" ")
+            cmd = parts[0]
+            args = parts[1:]
+            if cmd in ["q","quit"]:
+                print("Bye bye.")
+                break
+
+            if cmd in ["help","?","h"]:
+                self.printHelp()
+                
+            elif cmd in ["send"]:
+                self.sendMessage(args[0])
+
+            elif cmd in ["chan"]:
+                self.getChannel()
+
+            elif cmd in ["start"]:
+                self.startPolling()
+
+            elif cmd in ["stop"]:
+                self.stopPolling()
+
+        
 
 def main():
-    print ("Starting LogBot")
-    config = configparser.ConfigParser()
-    print("Loading config file: config.ini")
-    config.read('config.ini')
-    try:
-        apikey = config['default']['apikey']
-        if apikey == "":
-            print("Invalid config file.")
-            quit()
-    except Exception as e:
-        print("Error loading config file:"+e)
-        quit()
-        
-    api = WarcraftLogApi(apikey)
-    print(api.getFights("kFzACTYJbcmXLGqB"))
-    print(api.getReports(1499277098372).json())
-
+    LogBot().startCLI()
 
 if __name__ == "__main__":
     main()
